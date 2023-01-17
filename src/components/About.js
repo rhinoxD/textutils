24e02178c7d2f778f531b05bdda2a5256d5bbcb4
Shivam Kumar <coolshiv0721@gmail.com>
2023-01-17 21:24:26 +0530

    Fixed navbar dropdown for mobile and update about content

--- src/components/About.js
+++ src/components/About.js
@@ -26,7 +26,7 @@ const About = ({ mode }) => {
               aria-expanded='true'
               aria-controls='collapseOne'
             >
-              Accordion Item #1
+              <strong>Analyze your text</strong>
             </button>
           </h2>
           <div
@@ -40,14 +40,9 @@ const About = ({ mode }) => {
               bg-${mode === 'light' ? 'light' : 'dark'}
               text-${mode === 'light' ? 'dark' : 'light'}`}
             >
-              <strong>This is the first item's accordion body.</strong> It is
-              shown by default, until the collapse plugin adds the appropriate
-              classNamees that we use to style each element. These classNamees
-              control the overall appearance, as well as the showing and hiding
-              via CSS transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
+              TextUtils gives you a way to analyze your text quickly and
+              efficiently. Be it word count, character count, uppercase,
+              lowercase, titlecase that can be copied to clipboard.
             </div>
           </div>
         </div>
@@ -63,7 +58,7 @@ const About = ({ mode }) => {
               aria-expanded='false'
               aria-controls='collapseTwo'
             >
-              Accordion Item #2
+              <strong>Free to use</strong>
             </button>
           </h2>
           <div
@@ -77,14 +72,10 @@ const About = ({ mode }) => {
               bg-${mode === 'light' ? 'light' : 'dark'}
               text-${mode === 'light' ? 'dark' : 'light'}`}
             >
-              <strong>This is the second item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classNamees that we use to style each element. These classNamees
-              control the overall appearance, as well as the showing and hiding
-              via CSS transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
+              TextUtils is a free character counter tool that provides instant
+              character count & word count statistics for a given text.
+              TextUtils reports the number of words and characters. Thus it is
+              suitable for writing text with word/ character limit.
             </div>
           </div>
         </div>
@@ -100,7 +91,7 @@ const About = ({ mode }) => {
               aria-expanded='false'
               aria-controls='collapseThree'
             >
-              Accordion Item #3
+              <strong>Browser Compatible</strong>
             </button>
           </h2>
           <div
@@ -114,14 +105,10 @@ const About = ({ mode }) => {
               bg-${mode === 'light' ? 'light' : 'dark'}
               text-${mode === 'light' ? 'dark' : 'light'}`}
             >
-              <strong>This is the third item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classNamees that we use to style each element. These classNamees
-              control the overall appearance, as well as the showing and hiding
-              via CSS transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
+              This word counter software works in any web browsers such as
+              Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to
+              count characters in FaceBook, blog, books, excel document, pdf
+              document, essays, etc.
             </div>
           </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = ({ title, about, mode, toggleMode }) => {
           className='navbar-toggler'
           type='button'
           data-bs-toggle='collapse'
-          data-bs-target='/navbarSupportedContent'
+          data-bs-target='#navbarSupportedContent'
           aria-controls='navbarSupportedContent'
           aria-expanded='false'
           aria-label='Toggle navigation'
